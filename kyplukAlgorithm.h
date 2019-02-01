35a42bd7015bd1f39aa2db62ad05717d55cc6d88
kyplyk <avdosev1@yandex.ru>
2019-02-01 16:37:56 +0300

    fix spaces

--- kyplukAlgorithm.h
+++ kyplukAlgorithm.h
@@ -5,57 +5,57 @@
 
 namespace kypluk {
 
-template <class type>
-void swap (type& raz, type& dva) {
-	type temp = raz;
-	raz = dva;
-	dva = temp;
-}
-
-template <class type>
-int compare (const type& raz, const type& dva)
-{
-	if (raz > dva)
-		return 1;
-	elif (raz < dva)
-		return -1;
-	else
-		return 0;
-}
-
-template <class Iterator, class UnaryFunction>
-void for_each(Iterator begin, Iterator end, UnaryFunction func) {
-    for (auto it = begin; it != end; ++it) {
-	func(*it);
+    template <class type>
+    void swap (type& raz, type& dva) {
+        type temp = raz;
+        raz = dva;
+        dva = temp;
     }
-}
 
-template <class Iterator>
-uint distance(Iterator begin, Iterator end) {
-	uint res = 0;
-	while (begin != end) {
-		++begin;
-		res++;
-	}
-	return res;
-}
+    template <class type>
+    int compare (const type& raz, const type& dva)
+    {
+        if (raz > dva)
+            return 1;
+        elif (raz < dva)
+            return -1;
 
-template <class InputIterator, class OutputIterator>
-OutputIterator copy(InputIterator begin, InputIterator end, OutputIterator c_begin) {
-    while (begin != end) {
-	*c_begin++ = *begin++;
+        return 0;
     }
-    return c_begin;
-}
 
-template <class type>
-type max (type raz, type dva) {
-	return raz > dva ? raz : dva;
-}
-template <class type>
-type min (type raz, type dva) {
-	return raz < dva ? raz : dva;
-}
+    template <class Iterator, class UnaryFunction>
+    void for_each(Iterator begin, Iterator end, UnaryFunction func) {
+        for (auto it = begin; it != end; ++it) {
+            func(*it);
+        }
+    }
+
+    template <class Iterator>
+    uint distance(Iterator begin, Iterator end) {
+        uint res = 0;
+        while (begin != end) {
+            ++begin;
+            res++;
+        }
+        return res;
+    }
+
+    template <class InputIterator, class OutputIterator>
+    OutputIterator copy(InputIterator begin, InputIterator end, OutputIterator c_begin) {
+        while (begin != end) {
+            *c_begin++ = *begin++;
+        }
+        return c_begin;
+    }
+
+    template <class type>
+    type max (type raz, type dva) {
+        return raz > dva ? raz : dva;
+    }
+    template <class type>
+    type min (type raz, type dva) {
+        return raz < dva ? raz : dva;
+    }
 
 }
 #endif
