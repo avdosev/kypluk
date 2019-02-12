80e8a21c255aa37d44606a0a0213229cc3264801
kyplyk <avdosev1@yandex.ru>
2019-02-12 11:43:56 +0400

    fix vector

--- kyplukVector.h
+++ kyplukVector.h
@@ -2,118 +2,80 @@
 #define _kypluk_Vector_
 
 #include <kyplukDefine.h>
+#include <kyplukAlgorithm.h>
+#include <kyplukSmartPtr.h>
 
 template <class type>
 class Vector {
 	private:
-    //vector <type> name;
-        type *vector;
-        size_t size=1;
-        size_t realSize=1;
-        class vectorIterator;
-        friend class vectorIterator;
-
-
-        void birn() {
-            realSize= (realSize*3)/2 +1;
-            type* mem=new type[realSize];
-            memcpy(ret, vector, size*sizeof(type));
-            /*
-            ///
-            ///
-            */
-        }
+        type* arr;
+        size_t size;
+        size_t real_size;
+//        class vectorIterator;
+//        friend class vectorIterator;
+		void reborn() {
+			realSize = (realSize*3)/2 +1;
+	        type* temp = new type[real_size];
+	        copy(this->begin(), this->end(), temp);
+	        delete arr[];
+	        arr = temp;
+		}
+		
+		bool check_reborn(Size_t add_size) {
+			return (size+add_size > real_size);	
+		}
+		
 	public:
-        using Iterator = vectorIterator;
 
-        vector(size_t size=1) {
-            vector=new type[size];
-            this->size = realSize = size;
+        Vector(size_t size = 0, const type& value = type()) {
+            this->size = real_size = size;
+            arr = new type[size];
         }
 
-        void pushBack(const type& val);
-        void insert(int pos, type& value);
-        void insert(Iterator pos, type& value);
-        void resize(type count) {
-        void popBack();
-        void at(type pos);
-
-
-
+		void resize(Size_t count) { 
+            if (this->size < count) {
+        		this->size=count;
+	        } elif (this->size > count){
+	        	real_size = count;
+	        	type* temp = new type[real_size];
+	        	copy(this->begin(), this->end(), temp);
+	        }
+        }
+        
+        Size_t size() const {
+	        return this->size;
+	    }
+	
+	    void Vector::push_back(const type& val) {
+	    	while (check_reborn(1)) reborn();
+	        vector[size++] = val;
+	    }
+	
+	    void Vector::pop_back() {
+	    	if (!size)
+	        	arr[--size] = type();
+	    }
+	
+	    type& Vector::operator[] (Size_t pos)  {
+	        return arr[pos];
+	    }
+	
+	    const type& at(type pos) const {
+	        if (pos >= this->size) {
+	            throw 0;
+	        }
+	        else {
+	            return vector[pos];
+	        }
+	    }
+	    
+	    type& front() {
+	        return *begin();
+	    }
+	    
+	    type& back() {
+	    	return *end()-1;
+		}
 };
-
-
-
-    void Vector::pushBack(const type& val) {
-        size+=1;
-        vector[size] = val;
-    }
-
-    void Vector::insert(int pos, type& value) {
-    }
-
-    void Vector::insert(Iterator pos, type& value)
-    {
-
-    }
-
-    void Vector::resize(type count) {
-        if (this->size < count) {
-        this->size=count;
-        }
-        else {
-        //хм
-        }
-    }
-
-    void Vector::popBack() {
-        vector[this->size] = nullptr;
-        this->size-=1;
-
-    }
-
-    type& Vector::operator[] (type const& pos)  {
-        for (int it = 0; it<pos; ++it) {
-            if
-        }
-    }
-    const type& Vector::operator[](type const& pos ) const {
-
-    }
-
-    type& Vector::at(type pos) const {
-        if (!(pos < this->size)) {
-            throw "outOfRange";
-        }
-        else {
-            return vector[pos];//элемент на поз
-        }
-    }
-
-    type Vector::size() const {
-        return this->size;
-    }
-
-    type Vector::front() const {
-        return vector[0];
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #endif
