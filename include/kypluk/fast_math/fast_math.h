c013c3418af72eb91a8b49b3ea8d57a60ed8a594
avdosev <avdosev1@yandex.ru>
2020-08-27 14:28:36 +0400

    style fix: fast_math

--- include/kypluk/fast_math/fast_math.h
+++ include/kypluk/fast_math/fast_math.h
@@ -1,19 +1,22 @@
 #include <cstdint>
 
 namespace kypluk {
-    namespace fast_math {
-        float rsqrt( float number )
-        {
-            const float x2 = number * 0.5F;
-            const float threehalfs = 1.5F;
 
-            union {
-                float f;
-                uint32_t i;
-            } conv = {number}; // member 'f' set to value of 'number'.
-            conv.i  = 0x5f3759df - ( conv.i >> 1 );
-            conv.f  *= ( threehalfs - ( x2 * conv.f * conv.f ) );
-            return conv.f;
-        }
-    }
+namespace fast_math {
+
+inline float rsqrt( float number ) {
+    const float x2 = number * 0.5F;
+    const float threehalfs = 1.5F;
+
+    union {
+        float f;
+        uint32_t i;
+    } conv = {number}; // member 'f' set to value of 'number'.
+    conv.i  = 0x5f3759df - ( conv.i >> 1 );
+    conv.f  *= ( threehalfs - ( x2 * conv.f * conv.f ) );
+    return conv.f;
+}
+
+}
+
 }
